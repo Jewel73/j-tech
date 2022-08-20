3b2deb5656de79f2407d5ae99a5f0fb8a90b0f7f
jewel <jewelranar420@gmail.com>
2022-08-21 00:41:44 +0600

    completed spring jwt authentication

---
+++ src/main/java/com/jetch/web/entity/JwtResponse.java
@@ -0,0 +1,20 @@
+package com.jetch.web.entity;
+
+public class JwtResponse {
+    private String jwtToken;
+
+    public JwtResponse() {
+    }
+
+    public JwtResponse(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+
+    public String getJwtToken() {
+        return jwtToken;
+    }
+
+    public void setJwtToken(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+}
